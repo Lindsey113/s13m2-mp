39f78883aa0c9fc5963e107a66a201423e507d7d
Lindsey <lindsey_mccluskey@yahoo.ca>
2024-10-21 12:30:54 -0700

    wip

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -19,8 +19,21 @@ router.get('/', (req, res) => {
         }) 
 })
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', async (req, res) => {
+    try {
+        const postId = await Post.findById(req.params.id)
+        if(!postId){
+            res.status(404).json({
+                message: "The post with the specified ID does not exist"
+            })
+        } else {
+            res.status(200).json(postId)
+        }
+    } catch(err) {
+        res.status(500).json({
+            message: "The post information could not be retrieved"
+        })
+    }
 })
 
 router.post('/', (req, res) => {
